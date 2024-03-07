5613c469f3c50e5f321a439a94207b1e893ff815
mehrshadiy <mehrshadshakery@yahoo.com>
2024-03-07 23:36:06 +0330

    adding banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import {Banner, Section} from "@/components";
+
 export default function Home() {
   return (
     <>
-this is index
+        <Section >
+            <Banner title={'miss amazing grocery deals'}
+                    subtitle={'Sign up for the daily newsletter'}
+                    bgImage={'/assets/images/banner_bg.png'}
+                    image={'/assets/images/fresh-apples.png'}/>
+        </Section>
     </>
   );
 }
