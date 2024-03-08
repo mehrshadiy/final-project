8b10c030c1bb966544a49bb6528f34bd9f5b26d5
mehrshadiy <mehrshadshakery@yahoo.com>
2024-03-08 20:16:34 +0330

    implement SimpleProductSlider component

---
+++ src/components/common/product/simpleProductSlider/SimpleProductSlider.tsx
@@ -0,0 +1,51 @@
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Autoplay, Navigation} from "swiper/modules";
+import {SimpleProductCard} from "@/components";
+
+interface Props {
+    sliderData: Array<any>
+    nextEl?: string
+    prevEl?: string
+}
+
+export function SimpleProductSlider({sliderData, nextEl, prevEl}: Props) {
+    return (
+        <Swiper
+            spaceBetween={16}
+            slidesPerView={2}
+            modules={[Autoplay, Navigation]}
+            autoplay={true}
+            navigation={
+                {
+                    nextEl: nextEl,
+                    prevEl: prevEl
+                }
+            }
+            breakpoints={{
+                768: {
+                    slidesPerView: 3,
+                    spaceBetween: 18
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 22
+                },
+                1280: {
+                    slidesPerView: 5,
+                    spaceBetween: 24
+                }
+            }
+            }
+        >
+            {
+                sliderData.map((slideData, index) => {
+                    return (
+                        <SwiperSlide key={index}>
+                            <SimpleProductCard data={slideData}/>
+                        </SwiperSlide>
+                    )
+                })
+            }
+        </Swiper>
+    )
+}
