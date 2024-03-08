6a774762787c10fea0d9e618da97724246dc48e7
mehrshadiy <mehrshadshakery@yahoo.com>
2024-03-08 13:17:16 +0330

    implement Feature Categories component

---
+++ src/components/pages/homePage/featureCategories/FeatureCategories.tsx
@@ -0,0 +1,32 @@
+import {featuredCategoriesMock} from "@/mock/featuredCategories";
+import Link from "next/link";
+import {ImageView} from "@/components";
+
+
+export function FeatureCategories() {
+    return (
+        <>
+
+            <div className="flex flex-wrap justify-between gap-[24px]">
+
+                {
+                    featuredCategoriesMock.map((item, index) =>{
+                        return (
+                            <Link key={index} href={item.link} style={{backgroundColor: item.color}}
+                               className={"flex flex-col justify-center items-center text-blue-300 border-[1px] border-green-100 hover:border-green-300 px-2 lg:px-5 py-3 pt-2 rounded-xl"}>
+                                <ImageView src={item.img} width={93} height={84} alt={"cat"}
+                                     classname={"mb-2 lg:mb-4"}/>
+                                <h3 className="text-[12px] text-bold sm:text-heading-sm md:text-heading6 text-center">
+                                    {item.title}
+                                </h3>
+                                <span className="text-xsmall text-gray-400 hidden lg:flex">
+                                    {item.items} items
+                                </span>
+                            </Link>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import {Banner, Section} from "@/components";
+import {FeatureCategories} from "@/components/pages/homePage";
 
 export default function Home() {
-  return (
-    <>
-        <Section >
-            <Banner title={'miss amazing grocery deals'}
-                    subtitle={'Sign up for the daily newsletter'}
-                    bgImage={'/assets/images/banner_bg.png'}
-                    image={'/assets/images/fresh-apples.png'}/>
-        </Section>
-    </>
-  );
+    return (
+        <>
+            <Section>
+                <Banner title={'miss amazing grocery deals'}
+                        subtitle={'Sign up for the daily newsletter'}
+                        bgImage={'/assets/images/banner_bg.png'}
+                        image={'/assets/images/fresh-apples.png'}/>
+            </Section>
+            <Section>
+                <div className="hidden sm:flex mb-[50px]">
+                    <h2 className="text-heading3 text-blue-300">Featured Categories</h2>
+                </div>
+
+                <FeatureCategories/>
+            </Section>
+        </>
+    );
 }
