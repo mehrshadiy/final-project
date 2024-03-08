7ed1ea9129221585c3aff1d01864f0f14a8f4b3b
mehrshadiy <mehrshadshakery@yahoo.com>
2024-03-08 20:21:47 +0330

    implement BestSellersSlider component

---
+++ src/components/pages/homePage/bestSellersSlider/BestSellersSlider.tsx
@@ -0,0 +1,41 @@
+import {Autoplay} from "swiper/modules";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {SimpleProductCard} from "@/components";
+
+interface Props {
+    sliderData: Array<any>
+}
+
+export function BestSellersSlider({sliderData}: Props) {
+    return (
+        <>
+            <Swiper
+                spaceBetween={16}
+                slidesPerView={2}
+                modules={[Autoplay]}
+                autoplay={true}
+                breakpoints= {{
+                768: {
+                    slidesPerView: 3,
+                    spaceBetween: 18
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 22
+                }
+            }
+                }
+            >
+                {
+                    sliderData.map((slideData, index) => {
+                        return (
+                            <SwiperSlide key={index}>
+                                <SimpleProductCard data={slideData}/>
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
+        </>
+    )
+}
