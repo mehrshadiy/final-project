acd0f06e4b84765526ca22d63d73adffb89bea90
mehrshadiy <mehrshadshakery@yahoo.com>
2024-03-08 20:19:49 +0330

    implement IconBox component

--- src/components/common/ui/iconBox/IconBox.tsx
+++ src/components/common/ui/iconBox/IconBox.tsx
@@ -14,7 +14,7 @@ type Props = {
     linkClassName?: string
 };
 
-export function IconBox({icon, size = 22, link = '#', title, hideTitleOnMobile = false, badge = 0, titleClasName = "", path = 0, linkClassName}: Props) {
+export function IconBox({icon, size = 22, link, title, hideTitleOnMobile = false, badge = 0, titleClasName = "", path = 0, linkClassName}: Props) {
 
     let span = []
     for (let i = 1; i <= path; i++) {
@@ -23,11 +23,12 @@ export function IconBox({icon, size = 22, link = '#', title, hideTitleOnMobile =
         )
     }
 
-    return (
-        <Link href={link} className={`flex items-center cursor-pointer ${linkClassName}`}>
-            {
-                badge ?
-                    <div className="relative">
+    if (link){
+        return (
+            <Link href={'#'} className={`flex items-center cursor-pointer ${linkClassName}`}>
+                {
+                    badge ?
+                        <div className="relative">
                     <span
                         className="absolute -top-[10px] -right-[10px] w-[20px] h-[20px] bg-green-200 rounded-full flex justify-center items-center text-white text-xsmall">
                         {badge}
@@ -35,20 +36,46 @@ export function IconBox({icon, size = 22, link = '#', title, hideTitleOnMobile =
                             <i className={` ${icon} text-[${size}px]`}>
                                 {span}
                             </i>
-                    </div>
-                    :
+                        </div>
+                        :
                         <i className={` ${icon} text-[${size}px]`}>
                             {span}
                         </i>
-            }
+                }
 
-            {
-                title &&
-                <div
-                    className={` ${hideTitleOnMobile ? 'hidden xl:inline-block' : 'inline-block'} ${titleClasName} ml-1`}>{title}</div>
-            }
-        </Link>
+                {
+                    title &&
+                    <div
+                        className={` ${hideTitleOnMobile ? 'hidden xl:inline-block' : 'inline-block'} ${titleClasName} ml-1`}>{title}</div>
+                }
+            </Link>
+        )
+    }else {
+        return (
+            <>
+                {
+                    badge ?
+                        <div className="relative">
+                    <span
+                        className="absolute -top-[10px] -right-[10px] w-[20px] h-[20px] bg-green-200 rounded-full flex justify-center items-center text-white text-xsmall">
+                        {badge}
+                    </span>
+                            <i className={` ${icon} text-[${size}px]`}>
+                                {span}
+                            </i>
+                        </div>
+                        :
+                        <i className={` ${icon} text-[${size}px]`}>
+                            {span}
+                        </i>
+                }
 
-
-    )
+                {
+                    title &&
+                    <div
+                        className={` ${hideTitleOnMobile ? 'hidden xl:inline-block' : 'inline-block'} ${titleClasName} ml-1`}>{title}</div>
+                }
+            </>
+        )
+    }
 }
